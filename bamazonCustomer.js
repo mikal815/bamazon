d76ee46407c215cb2f71cc44dce16f6db39be81c
Mike M <mikal815@users.noreply.github.com>
2018-08-03 22:36:30 -0400

    finished the displayTable function using easy-table package

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,54 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+var mysql = require('mysql'); // node.js driver for mysql
+var inquirer = require('inquirer'); // Used for prompts on the command line
+var Table = require('easy-table'); // This renders the table nicely in the terminal
 
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection({  // Connects to the MySQL database
     host: 'localhost',
     user: 'root',
     password: 'root',
+    port: '3306',
     database: 'bamazon'
 });
 
 connection.connect();
+
+displayTable();
+
+function displayTable() {
+    connection.query("SELECT * FROM products", function(error, results) {
+
+        if (error) throw error;
+        console.log("\n\n");
+
+        console.log(`Welcome to Bamazon Musical Instruments!`)
+        console.log(``)
+
+        // The following block of code utilizes the "easy-table" package with it's variables, methods and functions
+         var t = new Table; 
+
+        // Displays the table found in the MySQL database
+        results.forEach(function(item) {
+        	t.cell('ID', item.id);
+        	t.cell('Product', item.product_name);
+        	t.cell('Department', item.department_name);
+        	t.cell('Price', item.price, Table.number(2));
+        	t.cell('Quantity', item.stock_quantity)
+        	t.newRow();  //Pushes the current row to the table and starts a new one
+
+
+        });
+
+        console.log(t.toString()); // Formats the table
+
+
+
+        
+    });
+}
+
+
+
+function exitProgram() {
+    connection.end();
+}
